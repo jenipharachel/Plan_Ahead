9be1912e2bd0bc34e9cef00cd53836f4f000a049
jenipha <jenipharachel@gmail.com>
2019-09-04 20:56:35 +0530

    node_modules you gotta commit, work with me

---
+++ build/precache-manifest.827ce2b0c12a52feed149d390f7d7926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebf8ae2df24d0b9cf56a",
+    "url": "/static/js/main.b71b8743.chunk.js"
+  },
+  {
+    "revision": "411cd85f605d19ee5f02",
+    "url": "/static/js/2.bbd364ed.chunk.js"
+  },
+  {
+    "revision": "ebf8ae2df24d0b9cf56a",
+    "url": "/static/css/main.162cc837.chunk.css"
+  },
+  {
+    "revision": "c7dbe3475ba2fe45c7a81c5bf17ee145",
+    "url": "/index.html"
+  }
+];
